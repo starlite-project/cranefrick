13527dfe37606c7852d7d8a4c2e3b82abc9d12a7
Owen Bellish <obellish@mpilabels.com>
2025-07-15 12:04:18 -0400

    more crates

---
+++ crates/hir/src/inner.rs
@@ -0,0 +1,22 @@
+use logos::Logos;
+
+/// This is here so we don't leak the [`Logos`] implementation.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Logos)]
+pub enum InnerOpCode {
+    #[token("<")]
+    MoveLeft,
+    #[token(">")]
+    MoveRight,
+    #[token("+")]
+    Increment,
+    #[token("-")]
+    Decrement,
+    #[token(",")]
+    Input,
+    #[token(".")]
+    Output,
+    #[token("[")]
+    StartLoop,
+    #[token("]")]
+    EndLoop,
+}
---
+++ crates/hir/src/lib.rs
@@ -0,0 +1,134 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
+#![no_std]
+
+extern crate alloc;
+
+mod inner;
+
+use alloc::vec::Vec;
+use core::{
+	error::Error as CoreError,
+	fmt::{Display, Formatter, Result as FmtResult, Write as _},
+	iter::once,
+};
+
+use logos::Lexer;
+use serde::{Deserialize, Serialize};
+use tracing::{debug, info};
+
+use self::inner::InnerOpCode;
+
+#[derive(Debug, Clone)]
+#[repr(transparent)]
+pub struct Parser<'source> {
+	inner: Lexer<'source, InnerOpCode>,
+}
+
+impl<'source> Parser<'source> {
+	pub fn new(source: &'source str) -> Self {
+		debug!("got source with length {}", source.len());
+
+		Self {
+			inner: Lexer::new(source),
+		}
+	}
+
+	pub fn parse<I>(self) -> Result<I, ParseError>
+	where
+		I: Default + Extend<BrainHir>,
+	{
+		info!("scanning {} chars", self.inner.source().len());
+
+		let mut result = I::default();
+
+		let mut bracket_stack = Vec::new();
+
+		for (i, op) in self.inner.filter_map(Result::ok).enumerate() {
+			let repr = match op {
+				InnerOpCode::MoveLeft => BrainHir::MovePtrLeft,
+				InnerOpCode::MoveRight => BrainHir::MovePtrRight,
+				InnerOpCode::Increment => BrainHir::IncrementCell,
+				InnerOpCode::Decrement => BrainHir::DecrementCell,
+				InnerOpCode::Input => BrainHir::GetInput,
+				InnerOpCode::Output => BrainHir::PutOutput,
+				InnerOpCode::StartLoop => {
+					bracket_stack.push(i);
+					BrainHir::StartLoop
+				}
+				InnerOpCode::EndLoop => match bracket_stack.pop() {
+					Some(_) => BrainHir::EndLoop,
+					None => return Err(ParseError(i)),
+				},
+			};
+
+			result.extend(once(repr));
+		}
+
+		Ok(result)
+	}
+}
+
+/// High-level intermediate representation. 1 to 1 for it's brainfuck equivalent.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum BrainHir {
+	/// Move the pointer left (<).
+	MovePtrLeft,
+	/// Move the pointer right (>).
+	MovePtrRight,
+	/// Increment the current cell (+).
+	IncrementCell,
+	/// Decrement the current cell (-).
+	DecrementCell,
+	/// Get the input from the input stream (,).
+	GetInput,
+	/// Put the current cell into the output stream (.).
+	PutOutput,
+	/// Start of a loop ([).
+	StartLoop,
+	/// End of a loop (]).
+	EndLoop,
+}
+
+impl Display for BrainHir {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		f.write_char(match *self {
+			Self::MovePtrLeft => '<',
+			Self::MovePtrRight => '>',
+			Self::IncrementCell => '+',
+			Self::DecrementCell => '-',
+			Self::GetInput => ',',
+			Self::PutOutput => '.',
+			Self::StartLoop => '[',
+			Self::EndLoop => ']',
+		})
+	}
+}
+
+#[derive(Debug)]
+pub struct ParseError(usize);
+
+impl Display for ParseError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		f.write_str("loop ending at #")?;
+		Display::fmt(&self.0, f)?;
+		f.write_str(" has no beginning")
+	}
+}
+
+impl CoreError for ParseError {}
+
+#[cfg(test)]
+mod tests {
+	use alloc::vec::Vec;
+
+	use super::{BrainHir, ParseError, Parser};
+
+	#[test]
+	fn basic_inc() -> Result<(), ParseError> {
+		let parsed = Parser::new("+++++").parse::<Vec<_>>()?;
+
+		assert_eq!(parsed, [BrainHir::IncrementCell; 5]);
+
+		Ok(())
+	}
+}
