0802b16f4dd95dfb26eeedd1bcab77056c6f9263
Owen Bellish <obellish@mpilabels.com>
2025-07-15 10:39:04 -0400

    more base files

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,1 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
