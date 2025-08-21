63e75cf0742ee474e123b2c962ce8b95e455bb8f
Owen Bellish <owenbellish@gmail.com>
2025-08-21 12:12:16 -0400

    add more flags

--- crates/llvm_assembler/src/inner/impls/loops.rs
+++ crates/llvm_assembler/src/inner/impls/loops.rs
@@ -6,33 +6,48 @@ use crate::{LlvmAssemblyError, inner::InnerAssembler};
 
 impl InnerAssembler<'_> {
 	pub fn dynamic_loop(&self, ops: &[BrainIr]) -> Result<(), AssemblyError<LlvmAssemblyError>> {
-        let head_block = self.context.append_basic_block(self.functions.main, "dynamic_loop head");
-        let body_block = self.context.append_basic_block(self.functions.main, "dynamic_loop body");
-        let next_block = self.context.append_basic_block(self.functions.main, "dynamic_loop next");
+		let head_block = self
+			.context
+			.append_basic_block(self.functions.main, "dynamic_loop head");
+		let body_block = self
+			.context
+			.append_basic_block(self.functions.main, "dynamic_loop body");
+		let next_block = self
+			.context
+			.append_basic_block(self.functions.main, "dynamic_loop next");
 
-        self.builder.build_unconditional_branch(head_block).map_err(AssemblyError::backend)?;
+		self.builder
+			.build_unconditional_branch(head_block)
+			.map_err(AssemblyError::backend)?;
 
-        self.builder.position_at_end(head_block);
+		self.builder.position_at_end(head_block);
 
-        let value = self.load(0)?;
+		let value = self.load(0)?;
 
-        let zero = {
-            let i8_type = self.context.i8_type();
+		let zero = {
+			let i8_type = self.context.i8_type();
 
-            i8_type.const_zero()
-        };
+			i8_type.const_zero()
+		};
 
-        let cmp =self.builder.build_int_compare(IntPredicate::NE, value, zero, "check pointer").map_err(AssemblyError::backend)?;
+		let cmp = self
+			.builder
+			.build_int_compare(IntPredicate::NE, value, zero, "check pointer")
+			.map_err(AssemblyError::backend)?;
 
-        self.builder.build_conditional_branch(cmp, body_block, next_block).map_err(AssemblyError::backend)?;
+		self.builder
+			.build_conditional_branch(cmp, body_block, next_block)
+			.map_err(AssemblyError::backend)?;
 
-        self.builder.position_at_end(body_block);
+		self.builder.position_at_end(body_block);
 
-        self.ops(ops)?;
+		self.ops(ops)?;
 
-        self.builder.build_unconditional_branch(head_block).map_err(AssemblyError::backend)?;
+		self.builder
+			.build_unconditional_branch(head_block)
+			.map_err(AssemblyError::backend)?;
 
-        self.builder.position_at_end(next_block);
+		self.builder.position_at_end(next_block);
 
 		Ok(())
 	}
