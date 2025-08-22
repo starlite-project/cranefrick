cad36e28cf9486416f5b30964e2fc9e64e22f359
Owen Bellish <owenbellish@gmail.com>
2025-08-22 13:49:57 -0400

    add attribute that matches rust

--- crates/llvm_assembler/src/inner/mod.rs
+++ crates/llvm_assembler/src/inner/mod.rs
@@ -123,45 +123,28 @@ impl<'ctx> Functions<'ctx> {
 		let ptr_type = context.default_ptr_type();
 		let void_type = context.void_type();
 
-		let nounwind_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("nounwind"), 0);
-
 		let noundef_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("noundef"), 1);
-		let noalias_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("noalias"), 2);
-		let nofree_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("nofree"), 3);
-		let nonnull_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("nonnull"), 4);
+			context.create_enum_attribute(Attribute::get_named_enum_kind_id("noundef"), 0);
+		let zeroext_attr =
+			context.create_enum_attribute(Attribute::get_named_enum_kind_id("zeroext"), 1);
+		let nocapture_attr =
+			context.create_enum_attribute(Attribute::get_named_enum_kind_id("nocapture"), 2);
 		let writeonly_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("writeonly"), 5);
-		let norecurse_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("norecurse"), 6);
-		let willreturn_attr =
-			context.create_enum_attribute(Attribute::get_named_enum_kind_id("willreturn"), 7);
+			context.create_enum_attribute(Attribute::get_named_enum_kind_id("writeonly"), 3);
 
 		let getchar_ty = void_type.fn_type(&[ptr_type.into()], false);
 		let getchar = module.add_function("getchar", getchar_ty, Some(Linkage::External));
 
-		getchar.add_attribute(AttributeLoc::Function, nounwind_attr);
-		getchar.add_attribute(AttributeLoc::Function, nofree_attr);
-		getchar.add_attribute(AttributeLoc::Function, norecurse_attr);
-		getchar.add_attribute(AttributeLoc::Function, willreturn_attr);
-		getchar.add_attribute(AttributeLoc::Param(0), noundef_attr);
-		getchar.add_attribute(AttributeLoc::Param(0), noalias_attr);
-		getchar.add_attribute(AttributeLoc::Param(0), nofree_attr);
-		getchar.add_attribute(AttributeLoc::Param(0), nonnull_attr);
-		getchar.add_attribute(AttributeLoc::Param(0), writeonly_attr);
+		for attr in [nocapture_attr, noundef_attr, writeonly_attr] {
+			getchar.add_attribute(AttributeLoc::Param(0), attr);
+		}
 
 		let putchar_ty = void_type.fn_type(&[i8_type.into()], false);
 		let putchar = module.add_function("putchar", putchar_ty, Some(Linkage::External));
 
-		putchar.add_attribute(AttributeLoc::Function, nounwind_attr);
-		putchar.add_attribute(AttributeLoc::Function, nofree_attr);
-		putchar.add_attribute(AttributeLoc::Function, norecurse_attr);
-		putchar.add_attribute(AttributeLoc::Function, willreturn_attr);
-		putchar.add_attribute(AttributeLoc::Param(0), noundef_attr);
+		for attr in [noundef_attr, zeroext_attr] {
+			putchar.add_attribute(AttributeLoc::Param(0), attr);
+		}
 
 		let main_ty = void_type.fn_type(&[], false);
 		let main = module.add_function("main", main_ty, Some(Linkage::External));
