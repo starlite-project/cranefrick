2437b3a89cfabd08f4b6b00598b75ccb3a5a89be
Owen Bellish <owenbellish@gmail.com>
2025-08-25 09:16:55 -0400

    bench more things

--- crates/ir/benches/parse.rs
+++ crates/ir/benches/parse.rs
@@ -7,6 +7,10 @@ const HELLO_WORLD: &str = include_str!("../../../programs/hello_world.bf");
 
 const HELLO_WORLD_TEST: &str = include_str!("../../../programs/tests/hello_world_test.bf");
 
+const MANDLEBROT: &str = include_str!("../../../programs/mandlebrot.bf");
+
+const AWIB: &str = include_str!("../../../programs/awib.bf");
+
 fn setup(source: &str) -> AstParser {
 	AstParser::new(
 		source
@@ -26,6 +30,18 @@ fn bench_basic(c: &mut Criterion) {
 	c.bench_function("hello_world_test parse", |b| {
 		b.iter(|| {
 			assert!(setup(black_box(HELLO_WORLD_TEST)).parse().is_ok());
+		});
+	});
+
+	c.bench_function("mandlebrot parse", |b| {
+		b.iter(|| {
+			assert!(setup(black_box(MANDLEBROT)).parse().is_ok());
+		});
+	});
+
+	c.bench_function("awib parse", |b| {
+		b.iter(|| {
+			assert!(setup(black_box(AWIB)).parse().is_ok());
 		});
 	});
 }
