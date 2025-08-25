56d712537f9896771e2f562c4c6876fbfdc8383f
Owen Bellish <owenbellish@gmail.com>
2025-08-25 09:53:43 -0400

    some cleanup

--- src/args.rs
+++ src/args.rs
@@ -24,36 +24,17 @@ pub enum Args {
 impl Args {
 	pub fn file_path(&self) -> &Path {
 		match self {
-			Self::Cranelift { file_path, .. } | Self::Llvm { file_path, .. } => file_path,
+			Self::Cranelift { file_path, .. } => file_path,
+			#[cfg(feature = "llvm")]
+			Self::Llvm { file_path, .. } => file_path,
 		}
 	}
 
 	pub fn output_path(&self) -> &Path {
 		match self {
-			Self::Cranelift { output_path, .. } | Self::Llvm { output_path, .. } => output_path,
+			Self::Cranelift { output_path, .. } => output_path,
+			#[cfg(feature = "llvm")]
+			Self::Llvm { output_path, .. } => output_path,
 		}
 	}
 }
-
-// #[derive(Debug, Parser)]
-// #[command(version, about, long_about = None)]
-// pub struct Args {
-// 	pub file_path: PathBuf,
-// 	#[arg(short, long)]
-// 	pub output_path: PathBuf,
-// 	#[arg(short, long)]
-// 	pub flags_path: Option<PathBuf>,
-// 	#[arg(short, long, default_value = "cranelift")]
-// 	pub assembler: AssemblerType,
-// }
-
-// #[derive(Debug, Clone, ValueEnum
-// )]
-// pub enum AssemblerType {
-// 	Cranelift {
-// 		flags_path: Option<PathBuf>
-// 	},
-// 	Llvm {
-// 		passes: Option<String>
-// 	}
-// }
