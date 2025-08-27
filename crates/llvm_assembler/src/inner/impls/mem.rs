8dd14f9ef0a537ae09a9ba60971acb7c3a24b4af
Owen Bellish <owenbellish@gmail.com>
2025-08-27 10:07:16 -0400

    don't type it as an array

--- crates/llvm_assembler/src/inner/impls/mem.rs
+++ crates/llvm_assembler/src/inner/impls/mem.rs
@@ -5,19 +5,12 @@ use crate::{LlvmAssemblyError, inner::InnerAssembler};
 impl<'ctx> InnerAssembler<'ctx> {
 	pub fn load(&self, offset: i32) -> Result<IntValue<'ctx>, LlvmAssemblyError> {
 		let i8_type = self.context.i8_type();
-		let i8_array_type = i8_type.array_type(30_000);
 
 		let current_offset = self.offset_ptr(offset)?;
 
-		let zero = self.ptr_type.const_zero();
-
 		let value = unsafe {
-			self.builder.build_in_bounds_gep(
-				i8_array_type,
-				self.tape,
-				&[zero, current_offset],
-				"load_gep",
-			)
+			self.builder
+				.build_in_bounds_gep(i8_type, self.tape, &[current_offset], "load_gep")
 		}?;
 
 		let loaded_value = self
@@ -40,19 +33,12 @@ impl<'ctx> InnerAssembler<'ctx> {
 
 	pub fn store(&self, value: IntValue<'ctx>, offset: i32) -> Result<(), LlvmAssemblyError> {
 		let i8_type = self.context.i8_type();
-		let i8_array_type = i8_type.array_type(30_000);
 
 		let current_offset = self.offset_ptr(offset)?;
 
-		let zero = self.ptr_type.const_zero();
-
 		let current_tape_value = unsafe {
-			self.builder.build_in_bounds_gep(
-				i8_array_type,
-				self.tape,
-				&[zero, current_offset],
-				"store_gep",
-			)
+			self.builder
+				.build_in_bounds_gep(i8_type, self.tape, &[current_offset], "store_gep")
 		}?;
 
 		let instr = self.builder.build_store(current_tape_value, value)?;
