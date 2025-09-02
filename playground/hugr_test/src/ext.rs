1f71797241cff59b765ab555fe9c1ed431b5bcf1
Owen Bellish <owenbellish@gmail.com>
2025-09-02 09:03:09 -0400

    more

--- playground/hugr_test/src/ext.rs
+++ playground/hugr_test/src/ext.rs
@@ -14,15 +14,7 @@ const VERSION: Version = Version::new(0, 0, 1);
 static EXTENSION: LazyLock<Arc<Extension>> = LazyLock::new(extension);
 
 fn extension() -> Arc<Extension> {
-	Extension::new_arc(ID, VERSION, |ext, extension_ref| {
-		ext.add_op(
-			OpName::new_inline("INC"),
-			"Increment current cell".to_owned(),
-			FuncValueType::new(vec![], vec![]),
-			extension_ref,
-		)
-		.unwrap();
-	})
+	Extension::new_arc(ID, VERSION, |ext, extension_ref| {})
 }
 
 fn get_op(op_name: &str) -> ExtensionOp {
