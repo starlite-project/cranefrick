18089f9980b67a21c1ba4d74e90b0ee093b0188d
Owen Bellish <owenbellish@gmail.com>
2025-09-02 09:26:54 -0400

    can't understand it

--- playground/hugr_test/src/main.rs
+++ playground/hugr_test/src/main.rs
@@ -1,13 +1,15 @@
 mod ext;
 
 use color_eyre::Result;
-use ext::inc_op;
 use hugr::{
 	Hugr,
 	algorithms::ComposablePass as _,
-	builder::{DFGBuilder, Dataflow, DataflowSubContainer, HugrBuilder, ModuleBuilder, inout_sig},
+	builder::{
+		DFGBuilder, Dataflow, DataflowHugr, DataflowSubContainer, HugrBuilder,
+		inout_sig,
+	},
 	extension::prelude::*,
-	std_extensions::logic::LogicOp,
+	ops::Value,
 	types::Signature,
 };
 use hugr_core::export::export_hugr;
@@ -17,9 +19,30 @@ fn main() -> Result<()> {
 	color_eyre::install()?;
 
 	let mut hugr = {
-		let mut dfg_builder = DFGBuilder::new(inout_sig(vec![], vec![]))?;
+		let mut dfg_builder = DFGBuilder::new(inout_sig(vec![], vec![bool_t()]))?;
 
-		dfg_builder.finish_hugr()
+		let new_defn = {
+			let mut mb = dfg_builder.module_root_builder();
+
+			let fb = mb.define_function("helper_id", Signature::new_endo(bool_t()))?;
+
+			let [f_inp] = fb.input_wires_arr();
+			fb.finish_with_outputs([f_inp])
+		}?;
+
+		let new_decl = dfg_builder
+			.module_root_builder()
+			.declare("helper2", Signature::new_endo(bool_t()).into())?;
+
+		let cst = dfg_builder.add_load_value(Value::true_val());
+
+		let [c1] = dfg_builder
+			.call(new_defn.handle(), &[], [cst])?
+			.outputs_arr();
+
+		let [c2] = dfg_builder.call(&new_decl, &[], [c1])?.outputs_arr();
+
+		dfg_builder.finish_hugr_with_outputs([c2])
 	}?;
 
 	print_hugr(&hugr);
