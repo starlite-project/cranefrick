161e9c216c3bf86e2c6ba1fb77901dc973911031
Owen Bellish <owenbellish@gmail.com>
2025-09-25 15:07:30 -0400

    better find zero implementation

--- crates/llvm_assembler/src/inner/impls/loops.rs
+++ crates/llvm_assembler/src/inner/impls/loops.rs
@@ -109,6 +109,16 @@ impl InnerAssembler<'_> {
 	}
 
 	pub fn find_zero(&self, offset: i32) -> Result<(), LlvmAssemblyError> {
+		let current_block = self.builder.get_insert_block().unwrap();
+
+		let ptr_int_type = self.ptr_int_type;
+		let i8_type = self.context().i8_type();
+
+		let current_pointer_value = self
+			.builder
+			.build_load(ptr_int_type, self.pointers.pointer, "find_zero_load")?
+			.into_int_value();
+
 		let header_block = self
 			.context()
 			.append_basic_block(self.functions.main, "find_zero.header");
@@ -123,13 +133,22 @@ impl InnerAssembler<'_> {
 
 		self.builder.position_at_end(header_block);
 
-		let value = self.load(0, "find_zero")?;
+		let header_phi_value = self.builder.build_phi(ptr_int_type, "find_zero_phi")?;
 
-		let zero = {
-			let i8_type = self.context().i8_type();
+		header_phi_value.add_incoming(&[(&current_pointer_value, current_block)]);
 
-			i8_type.const_zero()
-		};
+		let gep = self.gep(
+			i8_type,
+			header_phi_value.as_basic_value().into_int_value(),
+			"find_zero",
+		)?;
+
+		let value = self
+			.builder
+			.build_load(i8_type, gep, "find_zero_cell_load")?
+			.into_int_value();
+
+		let zero = i8_type.const_zero();
 
 		let cmp = self
 			.builder
@@ -140,11 +159,24 @@ impl InnerAssembler<'_> {
 
 		self.builder.position_at_end(body_block);
 
-		self.move_pointer(offset)?;
+		let offset_value = ptr_int_type.const_int(offset as u64, false);
+
+		let new_pointer_value = self.builder.build_int_add(
+			header_phi_value.as_basic_value().into_int_value(),
+			offset_value,
+			"find_zero_add",
+		)?;
 
 		self.builder.build_unconditional_branch(header_block)?;
 
+		header_phi_value.add_incoming(&[(&new_pointer_value, body_block)]);
+
 		self.builder.position_at_end(exit_block);
+
+		self.builder.build_store(
+			self.pointers.pointer,
+			header_phi_value.as_basic_value().into_int_value(),
+		)?;
 
 		Ok(())
 	}
