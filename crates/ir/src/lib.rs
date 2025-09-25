b8765860e5231f74aa3c2617d776f4c3c1c3bb2f
Owen Bellish <owenbellish@gmail.com>
2025-09-25 14:09:24 -0400

    don't want to double loop

--- crates/ir/src/lib.rs
+++ crates/ir/src/lib.rs
@@ -219,7 +219,11 @@ impl BrainIr {
 
 	#[must_use]
 	pub fn has_io(&self) -> bool {
-		self.has_output() || self.has_input()
+		if let Some(children) = self.child_ops() {
+			return children.iter().any(Self::has_io)
+		}
+
+		self.has_input() || self.has_output()
 	}
 
 	#[must_use]
