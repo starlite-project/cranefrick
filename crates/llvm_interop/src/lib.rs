60d861541890d319863013a008ebcf74e242fcc3
Owen Bellish <owenbellish@gmail.com>
2025-10-03 13:23:38 -0400

    don't trace in exception handler

--- crates/llvm_interop/src/lib.rs
+++ crates/llvm_interop/src/lib.rs
@@ -6,27 +6,14 @@ use std::{
 };
 
 unsafe extern "C" {
-	fn rust_eh_personality(
+	#[link_name = "rust_eh_personality"]
+	pub fn eh_personality(
 		version: i32,
 		actions: i32,
 		exception_class: i64,
 		exception_object: *mut u8,
 		context: *mut u8,
 	) -> i32;
-}
-
-#[unsafe(no_mangle)]
-#[tracing::instrument]
-pub unsafe extern "C" fn eh_personality(
-	version: i32,
-	actions: i32,
-	exception_class: i64,
-	exception_object: *mut u8,
-	context: *mut u8,
-) -> i32 {
-	tracing::error!("exception raised, unwinding");
-
-	unsafe { rust_eh_personality(version, actions, exception_class, exception_object, context) }
 }
 
 #[unsafe(no_mangle)]
