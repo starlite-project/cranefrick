9b579be5aafa0c119174df93a5c0a1b747a2d672
Owen Bellish <owenbellish@gmail.com>
2025-10-13 15:25:35 -0400

    cranelift was removed

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,7 @@ fn install_tracing(folder_path: &Path) {
 }
 
 fn env_filter() -> EnvFilter {
-	EnvFilter::new("info,cranelift_jit=warn")
+	EnvFilter::new("info")
 }
 
 fn serialize<T: Serialize>(value: &T, folder_path: &Path, file_name: &str) -> Result<()> {
