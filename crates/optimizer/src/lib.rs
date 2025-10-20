79cb5c0f4614d7f8860d65f1c7fd6565aec0090c
Owen Bellish <owenbellish@gmail.com>
2025-10-20 07:37:21 -0400

    more tracing

--- crates/optimizer/src/lib.rs
+++ crates/optimizer/src/lib.rs
@@ -68,163 +68,246 @@ impl Optimizer {
 	}
 
 	fn run_all_passes(&mut self, progress: &mut bool) {
-		let span = tracing::Span::current();
+		{
+			let _guard = self.pass_info("combine relavent instructions", 1);
+			run_with_span("optimize_consecutive_instructions", || {
+				*progress |= run_peephole_pass(self, passes::optimize_consecutive_instructions);
+			});
+		}
 
-		self.pass_info("combine relavent instructions");
-		let guard = trace_span!("optimize_consecutive_instructions").entered();
-		*progress |= run_peephole_pass(self, passes::optimize_consecutive_instructions);
-		span.pb_inc(1);
-		drop(guard);
+		{
+			let _guard = self.pass_info("add relative offsets", 1);
+			run_with_span("add_offsets", || {
+				*progress |= run_peephole_pass(self, passes::add_offsets);
+			});
+		}
 
-		self.pass_info("add relavent offsets");
-		let guard = trace_span!("add_offsets").entered();
-		*progress |= run_peephole_pass(self, passes::add_offsets);
-		span.pb_inc(1);
-		drop(guard);
+		{
+			let _guard = self.pass_info("fix boundary instructions", 2);
+			run_with_span("optimize_initial_sets", || {
+				*progress |= run_peephole_pass(self, passes::optimize_initial_sets);
+			});
+			run_with_span("fix_boundary_instructions", || {
+				*progress |= run_peephole_pass(self, passes::fix_boundary_instructions);
+			});
+		}
 
-		self.pass_info("fix boundary instructions");
-		let guard = trace_span!("optimize_initial_sets").entered();
-		*progress |= run_peephole_pass(self, passes::optimize_initial_sets);
-		drop(guard);
-		let guard = trace_span!("fix_boundary_instructions").entered();
-		*progress |= run_peephole_pass(self, passes::fix_boundary_instructions);
-		drop(guard);
-		span.pb_inc(2);
+		{
+			let _guard = self.pass_info("optimize clear cell instructions", 1);
+			run_with_span("clear_cell", || {
+				*progress |= run_loop_pass(self, passes::clear_cell);
+			});
+		}
 
-		self.pass_info("optimize clear cell instructions");
-		let guard = trace_span!("clear_cell").entered();
-		*progress |= run_loop_pass(self, passes::clear_cell);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("optimize set-based instructions", 1);
+			run_with_span("optimize_sets", || {
+				*progress |= run_peephole_pass(self, passes::optimize_sets);
+			});
+		}
 
-		self.pass_info("optimize set-based instructions");
-		let guard = trace_span!("optimize_sets").entered();
-		*progress |= run_peephole_pass(self, passes::optimize_sets);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("optimize find zero instructions", 1);
+			run_with_span("optimize_find_zero", || {
+				*progress |= run_loop_pass(self, passes::optimize_find_zero);
+			});
+		}
 
-		self.pass_info("optimize find zero instructions");
-		let guard = trace_span!("optimize_find_zero").entered();
-		*progress |= run_loop_pass(self, passes::optimize_find_zero);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("remove no-op instructions", 2);
+			run_with_span("remove_noop_instructions", || {
+				*progress |= run_peephole_pass(self, passes::remove_noop_instructions);
+			});
+			run_with_span("unroll_noop_loop", || {
+				*progress |= run_loop_pass(self, passes::unroll_noop_loop);
+			});
+		}
 
-		self.pass_info("remove no-op instructions");
-		let guard = trace_span!("remove_noop_instructions").entered();
-		*progress |= run_peephole_pass(self, passes::remove_noop_instructions);
-		drop(guard);
-		let guard = trace_span!("unroll_noop_loop").entered();
-		*progress |= run_loop_pass(self, passes::unroll_noop_loop);
-		drop(guard);
-		span.pb_inc(2);
+		{
+			let _guard = self.pass_info("remove unreachable loops", 1);
+			run_with_span("remove_unreachable_loops", || {
+				*progress |= run_peephole_pass(self, passes::remove_unreachable_loops);
+			});
+		}
 
-		self.pass_info("remove unreachable loops");
-		let guard = trace_span!("remove_unreachable_loops").entered();
-		*progress |= run_peephole_pass(self, passes::remove_unreachable_loops);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("remove infinite loops", 1);
+			run_with_span("remove_infinite_loops", || {
+				*progress |= run_loop_pass(self, passes::remove_infinite_loops);
+			});
+		}
 
-		self.pass_info("remove infinite loops");
-		let guard = trace_span!("remove_infinite_loops").entered();
-		*progress |= run_loop_pass(self, passes::remove_infinite_loops);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("remove empty loops", 1);
+			run_with_span("remove_empty_loops", || {
+				*progress |= run_loop_pass(self, passes::remove_empty_loops);
+			});
+		}
 
-		self.pass_info("remove empty loops");
-		let guard = trace_span!("remove_empty_loops").entered();
-		*progress |= run_loop_pass(self, passes::remove_empty_loops);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("unroll no-move dynamic loops", 1);
+			run_with_span("unroll_basic_dynamic_loop", || {
+				*progress |= run_peephole_pass(self, passes::unroll_basic_dynamic_loop);
+			});
+		}
 
-		self.pass_info("unroll no-move dynamic loops");
-		let guard = trace_span!("unroll_basic_dynamic_loop").entered();
-		*progress |= run_peephole_pass(self, passes::unroll_basic_dynamic_loop);
-		drop(guard);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("sort cell changes", 7);
+			run_with_span("sort_changes<8>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<8>);
+			});
+			run_with_span("sort_changes<7>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<7>);
+			});
+			run_with_span("sort_changes<6>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<6>);
+			});
+			run_with_span("sort_changes<5>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<5>);
+			});
+			run_with_span("sort_changes<4>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<4>);
+			});
+			run_with_span("sort_changes<3>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<3>);
+			});
+			run_with_span("sort_changes<2>", || {
+				*progress |= run_peephole_pass(self, passes::sort_changes::<2>);
+			});
+		}
 
-		self.pass_info("sort cell changes");
-		let guard = trace_span!("sort_changes", i = 8).entered();
-		*progress |= run_peephole_pass(self, passes::sort_changes::<8>);
-		drop(guard);
-		let guard = trace_span!("sort_changes", i = 7).entered();
-		*progress |= run_peephole_pass(self, passes::sort_changes::<7>);
-		drop(guard);
-		let guard = trace_span!("sort_changes", i = 6).entered();
-		*progress |= run_peephole_pass(self, passes::sort_changes::<6>);
-		drop(guard);
-				let guard = trace_span!("sort_changes", i = 5).entered();
-		*progress |= run_peephole_pass(self, passes::sort_changes::<5>);
-		drop(guard);
-				let guard = trace_span!("sort_changes", i = 4).entered();
-		*progress |= run_peephole_pass(self, passes::sort_changes::<4>);
-		drop(guard);
-				let guard = trace_span!("sort_changes", i = 3).entered();
-		*progress |= run_peephole_pass(self, passes::sort_changes::<3>);
-		drop(guard);
-		let guard = trace_span!()
-		*progress |= run_peephole_pass(self, passes::sort_changes::<2>);
-		span.pb_inc(7);
+		{
+			let _guard = self.pass_info("optimize scale and shift value instructions", 7);
+			run_with_span("optimize_move_value_from_loop", || {
+				*progress |= run_loop_pass(self, passes::optimize_move_value_from_loop);
+			});
+			run_with_span("optimize_move_value", || {
+				*progress |= run_peephole_pass(self, passes::optimize_move_value);
+			});
+			run_with_span("optimize_move_value_from_duplicate_cells", || {
+				*progress |=
+					run_peephole_pass(self, passes::optimize_move_value_from_duplicate_cells);
+			});
+			run_with_span("optimize_take_value", || {
+				*progress |= run_peephole_pass(self, passes::optimize_take_value);
+			});
+			run_with_span("optimize_fetch_value", || {
+				*progress |= run_peephole_pass(self, passes::optimize_fetch_value);
+			});
+			run_with_span("optimize_replace_value", || {
+				*progress |= run_peephole_pass(self, passes::optimize_replace_value);
+			});
+			run_with_span("optimize_copy_value", || {
+				*progress |= run_peephole_pass(self, passes::optimize_copy_value);
+			});
+		}
 
-		self.pass_info("optimize scale and shift value instructions");
-		*progress |= run_loop_pass(self, passes::optimize_move_value_from_loop);
-		*progress |= run_peephole_pass(self, passes::optimize_move_value);
-		*progress |= run_peephole_pass(self, passes::optimize_move_value_from_duplicate_cells);
-		*progress |= run_peephole_pass(self, passes::optimize_take_value);
-		*progress |= run_peephole_pass(self, passes::optimize_fetch_value);
-		*progress |= run_peephole_pass(self, passes::optimize_replace_value);
-		*progress |= run_peephole_pass(self, passes::optimize_copy_value);
-		span.pb_inc(7);
+		{
+			let _guard = self.pass_info("optimize write calls", 3);
+			run_with_span("optimize_writes", || {
+				*progress |= run_peephole_pass(self, passes::optimize_writes);
+			});
+			run_with_span("optimize_changes_and_writes", || {
+				*progress |= run_peephole_pass(self, passes::optimize_changes_and_writes);
+			});
+			run_with_span("optimize_offset_writes", || {
+				*progress |= run_peephole_pass(self, passes::optimize_offset_writes);
+			});
+		}
 
-		self.pass_info("optimize write calls");
-		*progress |= run_peephole_pass(self, passes::optimize_writes);
-		*progress |= run_peephole_pass(self, passes::optimize_changes_and_writes);
-		*progress |= run_peephole_pass(self, passes::optimize_offset_writes);
-		span.pb_inc(3);
+		{
+			let _guard = self.pass_info("remove redundant take instructions", 1);
+			run_with_span("remove_redundant_shifts", || {
+				*progress |= run_peephole_pass(self, passes::remove_redundant_shifts);
+			});
+		}
 
-		self.pass_info("remove redundant take instructions");
-		*progress |= run_peephole_pass(self, passes::remove_redundant_shifts);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("optimize constant shifts", 1);
+			run_with_span("optimize_constant_shifts", || {
+				*progress |= run_peephole_pass(self, passes::optimize_constant_shifts);
+			});
+		}
 
-		self.pass_info("optimize constant shifts");
-		*progress |= run_peephole_pass(self, passes::optimize_constant_shifts);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("remove unnecessary offsets", 1);
+			run_with_span("remove_offsets", || {
+				*progress |= run_peephole_pass(self, passes::remove_offsets);
+			});
+		}
 
-		self.pass_info("remove unnecessary offsets");
-		*progress |= run_peephole_pass(self, passes::remove_offsets);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("optimize sub cell", 3);
+			run_with_span("optimize_sub_cell_at", || {
+				*progress |= run_loop_pass(self, passes::optimize_sub_cell_at);
+			});
+			run_with_span("optimize_sub_cell_from", || {
+				*progress |= run_peephole_pass(self, passes::optimize_sub_cell_from);
+			});
+			run_with_span("optimize_sub_cell_from_with_set", || {
+				*progress |= run_peephole_pass(self, passes::optimize_sub_cell_from_with_set);
+			});
+		}
 
-		self.pass_info("optimize sub cell");
-		*progress |= run_loop_pass(self, passes::optimize_sub_cell_at);
-		*progress |= run_peephole_pass(self, passes::optimize_sub_cell_from);
-		*progress |= run_peephole_pass(self, passes::optimize_sub_cell_from_with_set);
-		span.pb_inc(3);
+		{
+			let _guard = self.pass_info("optimize if not zero", 2);
+			run_with_span("optimize_if_nz", || {
+				*progress |= run_loop_pass(self, passes::optimize_if_nz);
+			});
+			run_with_span("optimize_if_nz_when_zeroing", || {
+				*progress |= run_peephole_pass(self, passes::optimize_if_nz_when_zeroing);
+			});
+		}
 
-		self.pass_info("optimize if not zero");
-		*progress |= run_loop_pass(self, passes::optimize_if_nz);
-		*progress |= run_peephole_pass(self, passes::optimize_if_nz_when_zeroing);
-		span.pb_inc(2);
+		{
+			let _guard = self.pass_info("optimize duplicate cell", 3);
+			run_with_span("optimize_duplicate_cell", || {
+				*progress |= run_loop_pass(self, passes::optimize_duplicate_cell);
+			});
+			run_with_span("optimize_duplicate_cell_vectorization", || {
+				*progress |= run_peephole_pass(self, passes::optimize_duplicate_cell_vectorization);
+			});
+			run_with_span("unroll_constant_duplicate_cell", || {
+				*progress |= run_peephole_pass(self, passes::unroll_constant_duplicate_cell);
+			});
+		}
 
-		self.pass_info("optimize duplicate cell");
-		*progress |= run_loop_pass(self, passes::optimize_duplicate_cell);
-		*progress |= run_peephole_pass(self, passes::optimize_duplicate_cell_vectorization);
-		*progress |= run_peephole_pass(self, passes::unroll_constant_duplicate_cell);
-		span.pb_inc(3);
+		{
+			let _guard = self.pass_info("optimize memory operations", 2);
+			run_with_span("optimize_mem_sets", || {
+				*progress |= run_peephole_pass(self, passes::optimize_mem_sets);
+			});
+			run_with_span("optimize_mem_set_move_change", || {
+				*progress |= run_peephole_pass(self, passes::optimize_mem_set_move_change);
+			});
+		}
 
-		self.pass_info("optimize memory operations");
-		*progress |= run_peephole_pass(self, passes::optimize_mem_sets);
-		*progress |= run_peephole_pass(self, passes::optimize_mem_set_move_change);
-		span.pb_inc(2);
-
-		self.pass_info("unroll certain if nz");
-		*progress |= run_peephole_pass(self, passes::unroll_constant_if_nz);
-		span.pb_inc(1);
+		{
+			let _guard = self.pass_info("unroll certain if nz", 1);
+			run_with_span("unroll_constant_if_nz", || {
+				*progress |= run_peephole_pass(self, passes::unroll_constant_if_nz);
+			});
+		}
 	}
 
-	fn pass_info(&self, pass: &str) {
+	fn pass_info(&self, pass: &str, count: u64) -> impl Drop + use<> {
+		struct DropGuard {
+			count: u64,
+		}
+
+		impl Drop for DropGuard {
+			fn drop(&mut self) {
+				tracing::Span::current().pb_inc(self.count);
+			}
+		}
+
 		let (op_count, dloop_count, if_count) = self.stats();
 		debug!(
-			"running pass {pass} with {op_count} instructions ({dloop_count}) loops and {if_count} ifs"
+			"running pass{plural} to {pass} with {op_count} instructions, {dloop_count} loops and {if_count} ifs",
+			plural = if matches!(count, 1) { "" } else { "es" }
 		);
+
+		DropGuard { count }
 	}
 
 	fn stats(&self) -> (usize, usize, usize) {
@@ -295,6 +378,8 @@ impl FromIterator<BrainIr> for Optimizer {
 	}
 }
 
-fn run_with_span(name: &'static str) {
-	let span = trace_span!(target: name);
+fn run_with_span(pass_name: &'static str, mut f: impl FnMut()) {
+	let span = trace_span!("run_pass", pass = pass_name).entered();
+	f();
+	drop(span);
 }
