6f517296fbc3cd0c0fcf57fd2114869742a3478a
Owen Bellish <owenbellish@gmail.com>
2025-10-28 15:52:21 -0400

    new util to use in pass

---
+++ crates/utils/src/contains_range.rs
@@ -0,0 +1,36 @@
+use core::{
+	iter::Step,
+	ops::{Bound, RangeBounds},
+};
+
+pub trait ContainsRange<T> {
+	fn contains_range<R>(&self, other: &R) -> bool
+	where
+		R: RangeBounds<T>;
+}
+
+impl<T: Step, R> ContainsRange<T> for R
+where
+	R: RangeBounds<T>,
+{
+	fn contains_range<R2>(&self, other: &R2) -> bool
+	where
+		R2: RangeBounds<T>,
+	{
+		check(self.start_bound(), other.start_bound(), false)
+			&& check(other.end_bound(), self.end_bound(), true)
+	}
+}
+
+fn check<T: Step>(left: Bound<&T>, right: Bound<&T>, end: bool) -> bool {
+	match (left, right, end) {
+		(Bound::Unbounded, _, false) | (.., Bound::Unbounded, true) => true,
+		(.., Bound::Unbounded, false) | (Bound::Unbounded, _, true) => false,
+		(Bound::Included(l), Bound::Included(r), ..)
+		| (Bound::Excluded(l), Bound::Excluded(r), ..) => l <= r,
+		(Bound::Excluded(l), Bound::Included(r), false)
+		| (Bound::Included(l), Bound::Excluded(r), true) => l < r,
+		(Bound::Included(l), Bound::Excluded(r), false)
+		| (Bound::Excluded(l), Bound::Included(r), true) => l <= r || l == &Step::forward(r.clone(), 1),
+	}
+}
--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -4,11 +4,14 @@
 	feature(nonzero_internals),
 	allow(internal_features)
 )]
+#![cfg_attr(feature = "contains_range", feature(step_trait))]
 #![no_std]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+#[cfg(feature = "contains_range")]
+mod contains_range;
 #[cfg(any(feature = "unwrap_from", feature = "get_or_zero", feature = "convert"))]
 mod convert;
 #[cfg(feature = "insert_or_push")]
@@ -16,6 +19,8 @@ mod insert_or_push;
 #[cfg(feature = "iter")]
 mod iter;
 
+#[cfg(feature = "contains_range")]
+pub use self::contains_range::*;
 #[cfg(any(feature = "unwrap_from", feature = "get_or_zero", feature = "convert"))]
 pub use self::convert::*;
 #[cfg(feature = "insert_or_push")]
