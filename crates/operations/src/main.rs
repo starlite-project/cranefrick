ce9a5107c979eed6ef6082aa400e145910f017ac
Owen Bellish <owenbellish@gmail.com>
2025-11-11 10:01:42 -0500

    more parsing testing

--- crates/operations/src/main.rs
+++ crates/operations/src/main.rs
@@ -1,9 +1,10 @@
 use frick_operations::parse;
 
 fn main() {
-	let src = std::fs::read_to_string(std::env::args().nth(1).unwrap())
-		.unwrap()
-		.chars()
-		.collect::<String>();
+	// let src = std::fs::read_to_string(std::env::args().nth(1).unwrap())
+	// 	.unwrap()
+	// 	.chars()
+	// 	.collect::<String>();
+	let src = std::env::args().nth(1).unwrap();
 	println!("{:?}", parse(src).unwrap());
 }
--- crates/operations/src/parse.rs
+++ crates/operations/src/parse.rs
@@ -2,28 +2,34 @@ use alloc::{
 	string::{String, ToString as _},
 	vec::Vec,
 };
-use std::io;
+use std::{io, path::Path};
 
-use ariadne::{Color, IndexType, Label, Report, ReportKind, Source};
+use ariadne::{Color, FileCache, IndexType, Label, Report, ReportKind, Source, sources};
 use chumsky::prelude::*;
 
 use crate::{BrainOperation, BrainOperationType};
 
-pub fn parse(file_data: String) -> io::Result<Vec<BrainOperation>> {
+pub fn parse(file_path: impl AsRef<Path>) -> io::Result<Vec<BrainOperation>> {
+	let file_path = file_path.as_ref();
+
+	let file_name = file_path.file_name().and_then(|s| s.to_str()).unwrap();
+
+	let file_data = std::fs::read_to_string(file_path)?;
+
 	match parser().parse(file_data.as_str()).into_result() {
 		Ok(e) => Ok(e),
 		Err(errs) => {
 			for err in errs {
-				Report::build(ReportKind::Error, ((), err.span().into_range()))
+				Report::build(ReportKind::Error, (file_name, err.span().into_range()))
 					.with_config(ariadne::Config::new().with_index_type(IndexType::Byte))
 					.with_message(err.to_string())
 					.with_label(
-						Label::new(((), err.span().into_range()))
+						Label::new((file_name, err.span().into_range()))
 							.with_message(err.reason().to_string())
 							.with_color(Color::Red),
 					)
 					.finish()
-					.eprint(Source::from(&file_data))?;
+					.eprint((file_name, Source::from(&file_data)))?;
 			}
 
 			Ok(Vec::new())
