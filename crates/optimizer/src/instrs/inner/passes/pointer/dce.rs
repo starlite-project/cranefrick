949c7296953df245f05a2d9fdc7f6208ab608f9e
Owen Bellish <owenbellish@gmail.com>
2025-11-24 11:02:12 -0500

    use the mutability

--- crates/optimizer/src/instrs/inner/passes/pointer/dce.rs
+++ crates/optimizer/src/instrs/inner/passes/pointer/dce.rs
@@ -1,32 +1,28 @@
-use std::{
-	collections::BTreeMap,
-	ops::{Deref, DerefMut},
-};
-
 use frick_instructions::{BrainInstruction, BrainInstructionType};
 
 use super::{Pass, PointerAnalysis};
 
 #[derive(Debug, Default, Clone)]
-pub struct PointerDCEPass;
+#[repr(transparent)]
+pub struct PointerDCEPass {
+	analysis: PointerAnalysis,
+}
 
 impl Pass for PointerDCEPass {
 	fn run(&mut self, instrs: &mut Vec<BrainInstruction>) -> bool {
-		let mut analysis = PointerAnalysis::default();
-
-		if !analysis.analyze(instrs) {
+		if !self.analysis.analyze(instrs) {
 			tracing::debug!("no pointer analysis available");
 			return false;
 		}
 
-		let mut indices_to_remove = Vec::<usize>::with_capacity(analysis.len());
+		let mut indices_to_remove = Vec::<usize>::with_capacity(self.analysis.len());
 
 		for (i, instr) in instrs.iter().copied().enumerate().skip(1) {
 			if !matches!(instr.instr(), BrainInstructionType::LoadPointer) {
 				continue;
 			}
 
-			let prev_state = analysis.pointer_state_at(i - 1);
+			let prev_state = self.analysis.pointer_state_at(i - 1);
 
 			if !prev_state.value_known {
 				continue;
