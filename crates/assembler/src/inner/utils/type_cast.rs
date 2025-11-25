2ae9e172ba308a8a32d43390cd1c29cb1a8ef2d7
Owen Bellish <owenbellish@gmail.com>
2025-11-25 09:35:22 -0500

    use generics'

--- crates/assembler/src/inner/utils/type_cast.rs
+++ crates/assembler/src/inner/utils/type_cast.rs
@@ -1,7 +1,7 @@
 use inkwell::{
 	context::AsContextRef,
 	types::{BasicType, IntType},
-	values::{BasicValue, BasicValueEnum, IntValue},
+	values::{BasicValue, IntValue},
 };
 
 use crate::ContextGetter as _;
@@ -14,14 +14,14 @@ impl<'ctx> Castable<'ctx> for Bool {
 	type Type = IntType<'ctx>;
 	type Value = IntValue<'ctx>;
 
-	fn cast(v: BasicValueEnum<'ctx>) -> Self::Value {
-		v.into_int_value()
+	fn cast(v: impl BasicValue<'ctx>) -> Self::Value {
+		v.as_basic_value_enum().into_int_value()
 	}
 
-	fn assert_type_matches(v: BasicValueEnum<'ctx>, context: impl AsContextRef<'ctx>) {
+	fn assert_type_matches(v: impl BasicValue<'ctx>, context: impl AsContextRef<'ctx>) {
 		let bool_type = context.context().bool_type();
 
-		assert_eq!(v.get_type(), bool_type.into());
+		assert_eq!(v.as_basic_value_enum().get_type(), bool_type.into());
 	}
 }
 
@@ -32,14 +32,14 @@ impl<'ctx, const N: u32> Castable<'ctx> for Int<N> {
 	type Type = IntType<'ctx>;
 	type Value = IntValue<'ctx>;
 
-	fn cast(v: BasicValueEnum<'ctx>) -> Self::Value {
-		v.into_int_value()
+	fn cast(v: impl BasicValue<'ctx>) -> Self::Value {
+		v.as_basic_value_enum().into_int_value()
 	}
 
-	fn assert_type_matches(v: BasicValueEnum<'ctx>, context: impl AsContextRef<'ctx>) {
+	fn assert_type_matches(v: impl BasicValue<'ctx>, context: impl AsContextRef<'ctx>) {
 		let matching_ty = context.context().custom_width_int_type(N);
 
-		assert_eq!(v.get_type(), matching_ty.into());
+		assert_eq!(v.as_basic_value_enum().get_type(), matching_ty.into());
 	}
 }
 
@@ -48,7 +48,7 @@ pub trait Castable<'ctx> {
 
 	type Value: BasicValue<'ctx>;
 
-	fn cast(v: BasicValueEnum<'ctx>) -> Self::Value;
+	fn cast(v: impl BasicValue<'ctx>) -> Self::Value;
 
-	fn assert_type_matches(v: BasicValueEnum<'ctx>, context: impl AsContextRef<'ctx>);
+	fn assert_type_matches(v: impl BasicValue<'ctx>, context: impl AsContextRef<'ctx>);
 }
