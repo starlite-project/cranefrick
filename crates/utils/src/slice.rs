cd613058dd96e64bf2edcd8fac80b8cf0cf69687
Owen Bellish <owenbellish@gmail.com>
2025-12-01 11:15:01 -0500

    renaming

--- crates/utils/src/slice.rs
+++ crates/utils/src/slice.rs
@@ -2,42 +2,42 @@ use core::iter::FusedIterator;
 
 #[repr(transparent)]
 pub struct WindowsN<'a, T: 'a, const N: usize> {
-	v: &'a [T],
+	slice: &'a [T],
 }
 
 impl<'a, T: 'a, const N: usize> WindowsN<'a, T, N> {
 	const _LENGTH_CHECK: () = const { assert!(N != 0) };
 
 	const fn new(slice: &'a [T]) -> Self {
-		Self { v: slice }
+		Self { slice }
 	}
 }
 
 impl<'a, T: 'a, const N: usize> Clone for WindowsN<'a, T, N> {
 	fn clone(&self) -> Self {
-		Self { v: self.v }
+		Self { slice: self.slice }
 	}
 }
 
 impl<'a, T: 'a, const N: usize> DoubleEndedIterator for WindowsN<'a, T, N> {
 	fn next_back(&mut self) -> Option<Self::Item> {
-		if N > self.v.len() {
+		if N > self.slice.len() {
 			None
 		} else {
-			let ret = Some((&self.v[self.v.len() - N..]).try_into().ok()?);
-			self.v = &self.v[..self.v.len() - 1];
+			let ret = Some((&self.slice[self.slice.len() - N..]).try_into().ok()?);
+			self.slice = &self.slice[..self.slice.len() - 1];
 			ret
 		}
 	}
 
 	fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-		let (end, overflow) = self.v.len().overflowing_sub(n);
+		let (end, overflow) = self.slice.len().overflowing_sub(n);
 		if end < N || overflow {
-			self.v = &self.v[..0];
+			self.slice = &self.slice[..0];
 			None
 		} else {
-			let ret = (&self.v[end - N..end]).try_into().ok()?;
-			self.v = &self.v[..end - 1];
+			let ret = (&self.slice[end - N..end]).try_into().ok()?;
+			self.slice = &self.slice[..end - 1];
 			Some(ret)
 		}
 	}
@@ -51,20 +51,20 @@ impl<'a, T: 'a, const N: usize> Iterator for WindowsN<'a, T, N> {
 	type Item = &'a [T; N];
 
 	fn next(&mut self) -> Option<Self::Item> {
-		if N > self.v.len() {
+		if N > self.slice.len() {
 			None
 		} else {
-			let ret = Some((&self.v[..N]).try_into().ok()?);
-			self.v = &self.v[1..];
+			let ret = Some((&self.slice[..N]).try_into().ok()?);
+			self.slice = &self.slice[1..];
 			ret
 		}
 	}
 
 	fn size_hint(&self) -> (usize, Option<usize>) {
-		if N > self.v.len() {
+		if N > self.slice.len() {
 			(0, Some(0))
 		} else {
-			let size = self.v.len() - N + 1;
+			let size = self.slice.len() - N + 1;
 			(size, Some(size))
 		}
 	}
@@ -74,23 +74,23 @@ impl<'a, T: 'a, const N: usize> Iterator for WindowsN<'a, T, N> {
 	}
 
 	fn nth(&mut self, n: usize) -> Option<Self::Item> {
-		if let Some(rest) = self.v.get(n..)
+		if let Some(rest) = self.slice.get(n..)
 			&& let Some(nth) = rest.get(..N)
 		{
-			self.v = &rest[1..];
+			self.slice = &rest[1..];
 			Some(nth.try_into().ok()?)
 		} else {
-			self.v = &self.v[..0];
+			self.slice = &self.slice[..0];
 			None
 		}
 	}
 
 	fn last(self) -> Option<Self::Item> {
-		if N > self.v.len() {
+		if N > self.slice.len() {
 			None
 		} else {
-			let start = self.v.len() - N;
-			Some((&self.v[start..]).try_into().ok()?)
+			let start = self.slice.len() - N;
+			Some((&self.slice[start..]).try_into().ok()?)
 		}
 	}
 }
